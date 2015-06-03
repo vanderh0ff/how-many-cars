0782cbd4b5aaacf05f551d426c90e7c4e35ac80d
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2015-06-03 16:56:05 -0400

    generalized query_car_length

--- howmanycars.py
+++ howmanycars.py
@@ -15,8 +15,8 @@ def get_car_name():
     car = urllib.parse.urlencode({'terms':cartext})
     return [cartext,car]
 
-def query_car_length(carinfo):
-    data = se.get("http://carfolio.com/search/results/?"+carinfo[1]+"&offset=0&num=20&makematch=e&modelmatch=e")
+def query_car_length(se, car):
+    data = se.get("http://carfolio.com/search/results/?"+car+"&offset=0&num=20&makematch=e&modelmatch=e")
     doc = lxml.html.fromstring(data.content)
     cars = doc.xpath('//*[@id="content"]/div[2]/ol/li/dl/dt/a')
     data = se.get("http://carfolio.com"+cars[len(cars)-1].attrib['href'])
@@ -34,7 +34,7 @@ def get_route_points():
 
 def query_route_length(routepoints):
     mapsurl = "http://maps.googleapis.com/maps/api/directions/json?"
-    data = se.get(mapsurl+urllib.parse.urlencode(routepoints))
+    data = requests.get(mapsurl+urllib.parse.urlencode(routepoints))
     meters = data.json()['routes'][0]['legs'][0]['distance']['value']
     return meters
 
