78271f148f3ff606df41c4270c1e176f7bc39aa7
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2015-06-04 07:43:15 -0400

    function refactor working
    program now runs correctly

--- howmanycars.py
+++ howmanycars.py
@@ -1,5 +1,5 @@
 """
-Made by Matthew Vanderhoff, @vanderh0ff
+Made by Matthew Vander Hoff, @vanderh0ff
 """
 
 import requests
@@ -9,6 +9,7 @@ import lxml.html
 def init():
     se = requests.session()
     se.get("http://carfolio.com/search/results/?")
+    return se
 
 def get_car_name():
     cartext = input('what car?')
@@ -22,7 +23,7 @@ def query_car_length(se, car):
     data = se.get("http://carfolio.com"+cars[len(cars)-1].attrib['href'])
     doc = lxml.html.fromstring(data.content)
     length = doc.xpath('//*[@id="content"]/div[3]/table/tbody[2]/tr[6]/td[1]/strong')
-    return length[0].textcontent()
+    return length[0].text_content()
 
 def get_route_points():
     origin = input("start point ")
@@ -39,15 +40,17 @@ def query_route_length(routepoints):
     return meters
 
 def calculate_cars_on_route(routelength, carlength):
-    cartimes = routelength / (int(carlength)/1000)
+    return routelength / (int(carlength)/1000)
 
 
 def main():
-    init()
+    se = init()
     carinfo = get_car_name()
-    quert_car_length()
-    get_route_points()
-    query_route_length()
-    calculate_cars_on_route()
-    print ('a ', cartext, ' can fit on a route from ', origin, ' to ', destination,\
+    carlength = query_car_length(se, carinfo[1])
+    routepoints = get_route_points()
+    routelength = query_route_length(routepoints)
+    cartimes = calculate_cars_on_route(routelength, carlength)
+    print ('a ', carinfo[0], ' can fit on a route from ', routepoints['origin'], ' to ', routepoints['destination'],\
         ' ', cartimes, ' times.' )
+if __name__ == "__main__":
+    main()
